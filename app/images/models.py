cfa316a5bb0746b392c16d602a58400d1659d8f8
nicolasarquivo3 <nicolasarquivo3@gmail.com>
2026-08-25 07:19:20 -0300

    Update models.py

--- app/images/models.py
+++ app/images/models.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+import random
+import time
+
 
 @dataclass
 class ImageRequest:
@@ -9,6 +12,14 @@ class ImageRequest:
     width: int = 1024
     height: int = 1024
     reference_images: list[bytes] | None = None
+    seed: int | None = None
+    randomize_seed: bool = True
+
+    def __post_init__(self):
+        if self.randomize_seed or self.seed is None:
+            self.seed = int(time.time() * 1000) % 2_147_483_647
+            self.seed = (self.seed + random.randint(0, 999_999)) % 2_147_483_647
+
 
 @dataclass
 class ImageResult:
